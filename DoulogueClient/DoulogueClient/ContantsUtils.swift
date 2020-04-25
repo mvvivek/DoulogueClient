c25b8f08a03486e3932bf8175b304f0c6abe892c
vivek <m.vivek@onefc.com>
2020-04-26 01:18:36 +0530

    setup-framework

---
+++ DoulogueClient/DoulogueClient/ContantsUtils.swift
@@ -0,0 +1,15 @@
+//
+//  ContantsUtils.swift
+//  DoulogueClient
+//
+//  Created by vivek on 26/04/20.
+//  Copyright © 2020 vivek. All rights reserved.
+//
+
+import Foundation
+
+public class TestClass {
+    static public func greetMe() {
+        print("Hello vivek welcome to new world called programmings")
+    }
+}
---
+++ DoulogueClient/DoulogueClient/DoulogueClient.h
@@ -0,0 +1,19 @@
+//
+//  DoulogueClient.h
+//  DoulogueClient
+//
+//  Created by vivek on 26/04/20.
+//  Copyright © 2020 vivek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DoulogueClient.
+FOUNDATION_EXPORT double DoulogueClientVersionNumber;
+
+//! Project version string for DoulogueClient.
+FOUNDATION_EXPORT const unsigned char DoulogueClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DoulogueClient/PublicHeader.h>
+
+
