766b6b84cb83114592f8b04389fb7b937d4ad8c0
vivek <m.vivek@onefc.com>
2020-04-26 02:40:51 +0530

    test project added

---
+++ DoulougeTest/DoulougeTest/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  DoulougeTest
+//
+//  Created by vivek on 26/04/20.
+//  Copyright © 2020 vivek. All rights reserved.
+//
+
+import UIKit
+import DoulogueClient
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        TestClass.greetMe()
+    }
+}
+
